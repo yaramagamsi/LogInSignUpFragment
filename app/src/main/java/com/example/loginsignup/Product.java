0307d6418a414306b0d69fe08f4a86e1cadf826f
yaramagamsi <yaramagamsi@gmail.com>
2022-11-24 13:49:44 +0200

    Add product

---
+++ app/src/main/java/com/example/loginsignup/Product.java
@@ -0,0 +1,63 @@
+package com.example.loginsignup;
+
+public class Product {
+    private String category;
+    private String name;
+    private int price;
+    private String owner;
+    private String photo;
+
+    public Product(String category1, String s, String category, String name, String price)
+    {    }
+
+    public Product(String category, String name, int price, String   owner, String photo)
+    {
+        this.category = category;
+        this.name = name;
+        this.price = price;
+        this.owner = owner;
+        this.photo = photo;
+    }
+
+    public String getCategory()   {    return category;    }
+
+    public void setCategory (String category)
+    {  this.category = category;  }
+
+    public String getName()  {  return name;    }
+
+    public void setName(String name)  {  this.name = name;  }
+
+    public int getPrice()   {   return price; }
+
+    public void setPrice (int price)
+
+    {   this.price = price;  }
+
+
+    public String getOwner()   {  return owner;  }
+
+    public void setOwner (String owner)
+    {
+        this.owner = owner;
+    }
+
+    public String getPhoto()   {  return photo;  }
+
+    public void setPhoto(String photo)
+    {
+        this.photo = photo;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Product {" +
+                "category ='" + category + '\'' +
+                ", name='" + name + '\'' +
+                ", price ='" + price + '\'' +
+                ", owner ='" + owner + '\'' +
+                ", photo='" + photo +
+                '}';
+    }
+}
---
+++ app/src/main/java/com/example/loginsignup/ProductsFragments.java
@@ -0,0 +1,129 @@
+package com.example.loginsignup;
+
+import static android.content.ContentValues.TAG;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Spinner;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link ProductsFragments#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class ProductsFragments extends Fragment {
+
+    private EditText etName , etPrice;
+    private ImageView ivPhoto;
+    private Button btnAdd;
+    private Spinner spnCategory;
+    private FirebaseServices fbs;
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public ProductsFragments() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment ProductsFragments.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static ProductsFragments newInstance(String param1, String param2) {
+        ProductsFragments fragment = new ProductsFragments();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_products_fragments, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        connectComponents();
+    }
+
+    private void connectComponents() {
+
+        etName = getView().findViewById(R.id.etNameProducts);
+        etPrice = getView().findViewById(R.id.etPriceProducts);
+        ivPhoto = getView().findViewById(R.id.ivPhotoProducts);
+        btnAdd = getView().findViewById(R.id.btnAddProducts);
+        spnCategory = getView().findViewById(R.id.spnCategoryProducts);
+        fbs = FirebaseServices.getInstance();
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String name = etName.getText().toString();
+                String price = etPrice.getText().toString();
+                Integer.valueOf(price);
+                String category = spnCategory.getSelectedItem().toString();
+
+                // (String category, String name, int price, String owner, String phone)
+                Product p = new Product(category , name , price, "","");
+                Map<String, Product> books= new HashMap<>();
+
+                fbs.getFire().collection("books").document("LA")
+                        .set(p)
+                        .addOnSuccessListener(new OnSuccessListener<Void>() {
+                            @Override
+                            public void onSuccess(Void aVoid) {
+                                Log.d(TAG, "DocumentSnapshot successfully written!");
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Log.w(TAG, "Error writing document", e);
+                            }
+                        });
+            }
+        });
+    }
+}
